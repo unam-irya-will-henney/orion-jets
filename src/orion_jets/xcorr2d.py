38b08eda4feecd13f86f0c1c7bbb7843ac4b2600
William Henney <whenney@gmail.com>
2025-11-25 00:54:15 -0600

    Add in functions to find proper motions

--- src/orion_jets/xcorr2d.py
+++ src/orion_jets/xcorr2d.py
@@ -4,6 +4,9 @@ Two-dimensional cross correlation
 
 import numpy as np
 from scipy import signal
+from astropy.modeling import models, fitting
+
+FITTER = fitting.LMLSQFitter()
 
 
 def max_location_indices(arr: np.ndarray) -> tuple[int, ...]:
@@ -11,7 +14,13 @@ def max_location_indices(arr: np.ndarray) -> tuple[int, ...]:
 
     This behaves like maxloc() in Fortran
     """
-    return np.unravel_index(np.argmax(arr), arr.shape)
+    return np.unravel_index(np.argmax(arr), arr.shape)  # type: ignore
+
+
+def regularize(a):
+    mean = np.mean(a)
+    std = np.std(a)
+    return (a - mean) / std
 
 
 def measure_shift_integer(
@@ -30,15 +39,15 @@ def measure_shift_integer(
 
     # 2D cross-correlation: slide img_new over img_ref
     corr = signal.correlate2d(
-        img_ref,
-        img_new,
+        regularize(img_ref),
+        regularize(img_new),
         mode="full",
         boundary="fill",
         fillvalue=0.0,
     )
 
     # Location of maximum correlation
-    max_y, max_x = max_location_indice(corr)
+    max_y, max_x = max_location_indices(corr)
 
     # Zero-lag position in 'full' correlation
     # For correlate2d(ref, new): zero lag is at (ref_shape - 1)
@@ -52,4 +61,39 @@ def measure_shift_integer(
     return float(dy), float(dx)
 
 
-def xcorr_scipy(): ...
+def measure_shift_gfit(
+    img_ref: np.ndarray, img_new: np.ndarray
+) -> tuple[models.Gaussian2D, np.ndarray]:
+    """
+    Measure (dy, dx) that best aligns img_new onto img_ref using 2D cross-correlation with 2D Gaussian fitting.
+
+    Convention:
+        img_new(y + dy, x + dx) ≈ img_ref(y, x)
+    so positive dy, dx mean img_new must be shifted down/right to match img_ref.
+    """
+
+    # 2D cross-correlation: slide img_new over img_ref
+    corr = signal.correlate2d(
+        regularize(img_ref),
+        regularize(img_new),
+        mode="full",
+        boundary="fill",
+        fillvalue=0.0,
+    )
+    # Location of maximum correlation
+    max_y, max_x = max_location_indices(corr)
+
+    # Pixel grid for corr array indices
+    ny, nx = corr.shape
+    y, x = np.mgrid[0:ny, 0:nx]
+    # Fit a 2d gaussian to the cross-correlation image
+    g_init = models.Gaussian2D(
+        amplitude=1.0,
+        x_mean=max_x,
+        y_mean=max_y,
+        x_stddev=2.0,
+        y_stddev=2.0,
+    )
+    g = FITTER(g_init, x, y, corr)
+    # Return both the fit and the fitted image
+    return g, g(x, y)
